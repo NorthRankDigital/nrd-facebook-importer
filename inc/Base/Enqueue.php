ccd26a5233a5224f69f3780368e098ac4d26d67e
Jonathan Baird <jon@northrankdigital.com>
2024-07-31 21:31:10 -0500

    fixed css bug

--- inc/Base/Enqueue.php
+++ inc/Base/Enqueue.php
@@ -18,9 +18,9 @@ class Enqueue extends BaseController
   public function enqueue()
   {
     wp_enqueue_script('jquery');
-    wp_enqueue_style('nrdPluginCSS', $this->plugin_url . 'assets/css/nrd-fi-style.css');
-    wp_enqueue_script('nrdPluginJS', $this->plugin_url . 'assets/js/nrd-fi-script.js');
-    wp_localize_script('nrdPluginJS', 'ajax_object', array(
+    wp_enqueue_style('nrdfiPluginCSS', $this->plugin_url . 'assets/css/nrd-fi-style.css');
+    wp_enqueue_script('nrdfiPluginJS', $this->plugin_url . 'assets/js/nrd-fi-script.js');
+    wp_localize_script('nrdfiPluginJS', 'ajax_object', array(
         'ajax_url' => admin_url('admin-ajax.php'),
       )
     );
